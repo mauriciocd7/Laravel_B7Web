4ec2a1146e07a5384f093ebd2d8b5779b13f8d47
Mauricio Dias <mauriciod.dev@gmail.com>
2023-02-07 11:35:57 -0300

    How to use: ternary if

--- blade-laravel/resources/views/aulaBlade.blade.php
+++ blade-laravel/resources/views/aulaBlade.blade.php
@@ -7,10 +7,15 @@
     <title>Aula Blade</title>
 </head>
 <body>
-    <h4>Aula Blade</h4>
+    <h4>Aula Blade 1</h4>
     <h5>1+3 = {{2+2}}</h5> <!--Pode-se usar expressões no blade-->
     <h5>{{-- function()--}}</h5> <!--Comentários no blade é representado por dois hifens--> 
     <p>Código html: {{ $html }}</p> <!---->
     <p>Código html interpretado: {!! $html !!}</p> <!--Interpretar html no blade -->
+
+    <hr>
+    <h4>Aula Blade 2</h4>
+    <h5>{{$nome == 'teste' ? 'Sim' : 'Não'}}</h5> <!-- If ternário -->
+
 </body>
 </html>
